867f785b9bf03b147bcd365856045ac53177c94d
Konstantina Panagopoulou <151877044+konstantina44panag@users.noreply.github.com>
2024-04-26 17:05:22 +0300

    Update variables_v2.py

--- variables_v2.py
+++ variables_v2.py
@@ -107,7 +107,72 @@ agrr_sells_1min _after_930= agrr_sells_1min.between_time("09:30", "16:00")
 print(aggr_buys_1min_after_930)
 print(agrr_sells_1min _after_930)
 
+# 5.VWAP of trades (and separately buys/sells) over interval
+def custom_agg_function(data):
+    weighted_prices = (data["price"] * data["vol"]).sum()
+    total_volume = data["vol"].sum()
+
+    if total_volume == 0:
+        return 0.0
+    else:
+        return weighted_prices / total_volume
 
 
+vwap_trades_1min = trades.resample("1min", on="time").apply(custom_agg_function)
+vwap_trades_1min = vwap_trades_1min.rename("volume_weighted_price")
+
+vwap_buys_1min = Buys_trades.resample("1min", on="time").agg(custom_agg_function)
+vwap_buys_1min = vwap_buys_1min.rename("volume_weighted_price")
+
+vwap_sells_1min = Sells_trades.resample("1min", on="time").agg(custom_agg_function)
+vwap_sells_1min = vwap_sells_1min.rename("volume_weighted_price")
+
+vwap_trades_1min_after_930 = vwap_trades_1min.between_time("09:30", "16:00")
+vwap_buys_1min_after_930 = vwap_buys_1min.between_time("09:30", "16:00")
+vwap_sells_1min_after_930 = vwap_sells_1min.between_time("09:30", "16:00")
+
+print(vwap_trades_1min_after_930)
+print(vwap_buys_1min_after_930)
+print(vwap_sells_1min_after_930)
 
 
+# 6. Simple average of trade prices (and separately buys/sells) over interval
+average_trades_1min = trades.resample("1min", on="time").agg({"price": "mean"})
+average_buys_1min = Buys_trades.resample("1min", on="time").agg({"price": "mean"})
+average_sells_1min = Sells_trades.resample("1min", on="time").agg({"price": "mean"})
+
+average_trades_1min_after_930 = average_trades_1min.between_time("09:30", "16:00")
+average_buys_1min_after_930 = average_buys_1min.between_time("09:30", "16:00")
+average_sells_1min_after_930 = average_sells_1min.between_time("09:30", "16:00")
+
+print(vwap_trades_1min_after_930)
+print(average_buys_1min_after_930)
+print(average_sells_1min_after_930)
+
+# 8. Time weighted version of bid/ask prices and size (in dollars) of best bid and ask
+def calculate_twap_and_volume(group):
+    durations = (
+        group.index.to_series()
+        .diff()
+        .fillna(pd.Timedelta(seconds=0))
+        .dt.total_seconds()
+    )
+    time_weighted_prices = group["price"] * durations
+    time_weighted_volumes = group["vol"] * durations
+    total_time = durations.sum()
+    twap = time_weighted_prices.sum() / total_time if total_time != 0 else 0
+    twav = time_weighted_volumes.sum() / total_time if total_time != 0 else 0
+
+    return pd.Series({"TWAP": twap, "TWAV": twav})
+
+twap_trades = trades.resample("1min").apply(calculate_twap_and_volume)
+twap_asks = Ask.resample("1min").apply(calculate_twap_and_volume)
+twap_bids = Bid.resample("1min").apply(calculate_twap_and_volume)
+
+twap_trades_after_930 = twap_trades.between_time("09:30", "16:00")
+twap_asks_after_930 = twap_asks.between_time("09:30", "16:00")
+twap_bids_after_930 = twap_bids.between_time("09:30", "16:00")
+
+print(twap_trades_after_930)
+print(twap_asks_after_930)
+print(twap_bids_after_930)
